fd575f963b5ddc9dde91975fb774d300088dd8e5
Hatchell <82800783+Hatchell@users.noreply.github.com>
2021-04-19 02:21:48 -0500

    Add files via upload

---
+++ RandomizerArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApp2
+{
+    class RandomizerArray
+    {
+        static Random _randomgen = new Random();
+
+        public static int[] RandomizeStrings(int[] arr)
+        {
+            List<KeyValuePair<int, int>> list = new List<KeyValuePair<int, int>>();
+            // Agregar valores al array
+
+            foreach (int s in arr)
+            {
+                list.Add(new KeyValuePair<int, int>(_randomgen.Next(), s));
+            }
+            // Randomizar lista por numero aleatorio
+            var sorted = from item in list
+                         orderby item.Key
+                         select item;
+            // introducir valores al nuevo array
+            int[] result = new int[arr.Length];
+            // copiar valores al array
+            int index = 0;
+            foreach (KeyValuePair<int, int> pair in sorted)
+            {
+                result[index] = pair.Value;
+                index++;
+            }
+
+            return result;
+        }
+    }
+}
